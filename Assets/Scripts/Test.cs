807478d7352545b60fe9d3740a0b581616bf7e8d
Hannu Suhonen <han.suhonen@icloud.com>
2019-12-26 00:53:21 +0200

    Initial Commit.

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(HelloWorld());
+    }
+
+    IEnumerator HelloWorld()
+    {
+        Debug.Log("Wow, what is this!?");
+        yield return new WaitForSeconds(5f);
+        Debug.Log("Hello World!");
+    }
+}
