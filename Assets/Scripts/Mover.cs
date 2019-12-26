73efb24d0b8edb1c7311db863668f9477270f5ee
Hannu Suhonen <han.suhonen@icloud.com>
2019-12-26 16:40:39 +0200

    Implemented navmesh

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
