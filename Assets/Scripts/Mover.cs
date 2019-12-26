af13a10cddea2e1ad74731514d51f2d0842ec139
Hannu Suhonen <han.suhonen@icloud.com>
2019-12-26 18:59:26 +0200

    Created navmesh navigation for player.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,11 +6,23 @@ using UnityEngine.UI;
 
 public class Mover : MonoBehaviour
 {
-
-    [SerializeField] Transform target;
-
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray,out hit);
+        if(hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+        
     }
 }
