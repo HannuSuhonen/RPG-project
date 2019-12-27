ba6c12efd306274238bd464e63e3bfb4ffe06b83
Hannu Suhonen <han.suhonen@icloud.com>
2019-12-28 01:02:34 +0200

    Updated FollowCamera script for delayed follow. Reorganised folder structure.

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class FollowCamera : MonoBehaviour
+{
+    
+    [SerializeField] Transform target;
+    [SerializeField] float cameraSpeed = 0;
+    Vector3 offset;
+    
+    private void Start() 
+    {
+        offset = transform.position - target.position;
+    }
+    void LateUpdate() //Called after all other updates are called, used to prevent camera jump bug. Check unity execution order for more details.
+    {
+        //transform.position = target.position;
+        Vector3 velocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp
+        (transform.position,target.position + offset, ref velocity,cameraSpeed);
+    }
+}
