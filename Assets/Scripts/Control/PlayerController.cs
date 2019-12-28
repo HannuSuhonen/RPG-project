2345095b373e67d3844ca725280ec12fd485e8b8
Hannu Suhonen <han.suhonen@icloud.com>
2019-12-28 20:59:21 +0200

    Refactored mover script into 2 seperate scripts for better control over raycasting.

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+    
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit; //Unassigned variable that we will use to store: "OUT" raycastHit data, in the next line ->
+        bool hasHit = Physics.Raycast(ray,out hit);
+        if(hasHit)
+        {
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -9,22 +9,12 @@ public class Mover : MonoBehaviour
 {
     void Update()
     {
-        if(Input.GetMouseButton(0))
-        {
-            MoveToCursor();
-        }
         UpdateAnimator();
     }
 
-    private void MoveToCursor()
+    public void MoveTo(Vector3 destination)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit; //Unassigned variable that we will use to store: "OUT" raycastHit data, in the next line ->
-        bool hasHit = Physics.Raycast(ray,out hit);
-        if(hasHit)
-        {
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
+        GetComponent<NavMeshAgent>().destination = destination;
     }
 
     void UpdateAnimator()
